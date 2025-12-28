bdf997938baeb18f04d7dd7ba10bdae40dad8630
Minhaj Haider <smhaider1808@gmail.com>
2025-12-28 20:50:56 +0500

    image optimize turned off

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
     domains: ["images.unsplash.com"],
     remotePatterns: [
        { hostname: "images.unsplash.com"},
-    ]
+    ],
+    unoptimized: true,
   },
 };
 
